b5518474a482bbd389a1c7fbb289d434a3631d94
Noel Georgi <git@frezbo.dev>
2021-01-11 02:32:13 +0530

    Fix CI
    Signed-off-by: Noel Georgi <git@frezbo.dev>

--- bitwarden_rs/test.sh
+++ bitwarden_rs/test.sh
@@ -15,8 +15,6 @@ DATA_DIR="${TESTDIR}/${DATA_DIR_NAME}"
 
 mkdir -p "${DATA_DIR}"
 
-[[ -v GITHUB_ACTIONS ]] && sudo chown 1000:1000 "${DATA_DIR}"
-
 function cleanup() {
     docker container logs "${CONTAINER_NAME}"
     docker container rm --force "${CONTAINER_NAME}"
@@ -32,6 +30,8 @@ trap cleanup EXIT SIGINT
     openssl genrsa -out "${DATA_DIR}/rsa_key.pem" 4096
     openssl rsa -in "${DATA_DIR}/rsa_key.pem" -outform DER -out "${DATA_DIR}/rsa_key.der"
     openssl rsa -in "${DATA_DIR}/rsa_key.der" -inform DER -RSAPublicKey_out -outform DER -out "${DATA_DIR}/rsa_key.pub.der"
+    # change ownership of data directory when running in github actions
+    [[ -v GITHUB_ACTIONS ]] && sudo chown -R 1000:1000 "${DATA_DIR}"
     docker container run --name "${CONTAINER_NAME}" --detach --publish 8080:8080 --volume "${DATA_DIR}/:/${DATA_DIR_NAME}/" "${IMAGE}"
     # wait for container to boot
     sleep 10
