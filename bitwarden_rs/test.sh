062162f58ddcb4191cbda7007fc8243a51122324
Noel Georgi <git@frezbo.dev>
2021-01-11 01:20:12 +0530

    Add testing
    Signed-off-by: Noel Georgi <git@frezbo.dev>

--- bitwarden_rs/test.sh
+++ bitwarden_rs/test.sh
@@ -4,22 +4,28 @@ set -eu -o pipefail
 
 trap cleanup EXIT SIGINT
 
-mkdir -p bitwarden_rs
-
 CONTAINER_NAME=$(uuidgen)
+REPO="ghcr.io/frezbo"
+
+PROJECT="${1}"
+TAG="${2}"
+
+IMAGE="${REPO}/${PROJECT}:${TAG}"
+
+mkdir -p "${PROJECT}"
 
 (
-    cd bitwarden_rs
+    cd "${PROJECT}"
     # generate keys for bitwarden
     mkdir -p data
     openssl genrsa 4096 -out data/rsa_key.pem
     openssl rsa -in data/rsa_key.pem -outform DER -out data/rsa_key.der
     openssl rsa -in data/rsa_key.pem -inform DER -RSAPublicKey_out -outform DER -out data/rsa_key.pub.der
-    docker container run --name "${CONTAINER_NAME}" --detach--publish 8080:8080 --volume "${PWD}/data/:/data/" ghcr.io/frezbo/bitwarden_rs
-    curl -sf http://localhost:8080/alive
+    docker container run --name "${CONTAINER_NAME}" --detach--publish 8080:8080 --volume "${PWD}/data/:/data/" "${IMAGE}"
+    curl -sf http://localhost:8080/alive || docker container logs "${CONTAINER_NAME}"
 )
 
 function cleanup() {
     docker container rm --force "${CONTAINER_NAME}"
-    rm -rf bitwarden_rs
+    rm -rf "${PROJECT}"
 }
